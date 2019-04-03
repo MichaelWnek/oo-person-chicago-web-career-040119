b3e4176aecf4c426ac3d09e82b56e2286e87bf48
Michael Wnek <michaelwnek1@gmail.com>
2019-04-03 19:47:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,7 @@
-
 class Person
 
-  attr_reader :name, :happiness, :hygiene
   attr_accessor :bank_account
-
-  PERSON = []
+  attr_reader :name, :happiness, :hygiene
 
   def initialize(name)
     @name = name
@@ -13,96 +10,61 @@ class Person
     @hygiene = 8
   end
 
-# syntax for writer when defining a method involves "="
   def happiness=(num)
-    if num > 10
-      @happiness = 10
-    elsif num < 0
-      @happiness = 0
-    else
-      @happiness = num
-    end
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
   end
-    # @happiness = num
-    # @happiness = 10 if @happiness > 10
-    # @happiness = 0 if @happiness < 0
 
-    def hygiene=(num)
-      if num > 10
-        @happiness = 10
-      elsif num < 0
-        @happiness = 0
-      else
-        @happiness = num
-      end
-    end
-
-
-  # def hygiene=(num)
-  #   @hygiene = num
-  #   @hygiene = 10 if @happiness > 10
-  #   @hygiene = 0 if @happiness < 0
-  # end
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
 
   def happy?
-    if happiness > 7
-      return True
-    end 
-  end 
-
-
-
-  def clean?
-    if hygiene > 7
-      return True
-    end 
-  end 
-
-
-
-  def get_paid(salary)
-    salary += @bank_account
-    return "all about the benjamins"
-  end 
-
-  def take_bath
-    #talking about the same @hygiene variable
-    self.hygiene += 4
-    return "Rub-a-dub just relaxing in the tub"
+    happiness > 7
   end
 
+  def clean?
+    hygiene > 7
+  end
 
-    def work_out
-      self.hygiene -= 3
-      self.happiness += 2 
-      return "another one bites the dust"
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def start_conversation(person, topic)
+    objects = [self, person]
+    if topic == "politics"
+      objects.each { |o| o.happiness -= 2}
+      first, second = ["partisan", "lobbyist"]
+    elsif topic == "weather"
+      objects.each { |o| o.happiness += 1}
+      first, second = ["sun", "rain"]
     end
+    first ||= "blah"
+    second ||= "blah"
+    base_string = "blah blah #{first} blah #{second}"
+  end
 
-    def call_friend(friend)
-        self.happiness += 3
-        friend.happiness += 3
-        return "Hi #{friend.name}! It's #{self.name}. How are you?"
-    end
+  def get_paid(salary)
+    self.bank_account += salary
+    self.happiness += 1
+    "all about the benjamins"
+  end
 
-    def start_conversation(person, topic)
-      if topic == "politics"
-        self.happiness -= 2
-        person.happiness -= 2
-        return "blah blah partisan blah lobbyist"
-      elsif topic == "weather"
-        self.happiness += 1
-        person.happiness += 1
-        return "blah blah sun blah rain"
-      else 
-        self.happiness += 0 
-        person.happiness += 0
-         return "blah blah blah blah blah"
-       end 
-     end
-        
-        
-      
+  def call_friend(friend)
+    [friend, self].each {|o| o.happiness += 3 }
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
 
-    
 end
 
